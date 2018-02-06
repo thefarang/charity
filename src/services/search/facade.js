b84cef102cf31243ae8e8f19e98a8627a1706fc1
thefarang <benjamin.vickers@yahoo.co.uk>
2018-02-06 09:36:51 +0000

    update

--- src/services/search/facade.js
+++ src/services/search/facade.js
@@ -1,17 +1,22 @@
 'use strict'
 
+/*
 const config = require('config')
 const elasticsearch = require('elasticsearch')
 
 const servLog = require('../log')
 const charityActions = require('./actions/charities')
+*/
 
+/*
 const esClient = new elasticsearch.Client({
   host: 'http://search:9200',
   log: 'error'
 })
+*/
 
 const searchKeyword = async (keyword) => {
+  /*
   const index = 'keyword'
   const body = {
     from: 0,
@@ -25,16 +30,14 @@ const searchKeyword = async (keyword) => {
     const results = await esClient.search({ index: index, body: body })
     console.log(`found ${results.hits.total} items in ${results.took}ms`);
     console.log(`returned keywords:`);
-    /*
     results.hits.hits.forEach(
       (hit, index) => console.log(
         `\t${body.from + ++index} - ${hit._source.title}`
       )
     )
-    */
   } catch (err) {
-
   }
+  */
 }
 
 const searchCauseName = (name) => {
